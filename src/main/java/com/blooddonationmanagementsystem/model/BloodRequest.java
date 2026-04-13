5f31a90230ce649bddfe6cbfe77e791cf8954d2b
diyasiwakoti-12 <diyasiwakoti59@gmail.com>
2026-04-13 09:23:33 +0545

    Created the getter setter method in BloodRequest.java

--- src/main/java/com/blooddonationmanagementsystem/model/BloodRequest.java
+++ src/main/java/com/blooddonationmanagementsystem/model/BloodRequest.java
@@ -1,4 +1,57 @@
 package com.blooddonationmanagementsystem.model;
 
+
+
 public class BloodRequest {
+
+    private int id;
+    private int patientId;
+    private String bloodGroup;
+    private int quantity;
+    private String urgency;
+    private String status;
+    private String requestDate;
+    private String patientName;
+
+
+    public int getId() { 
+        return id; 
+    }
+
+    public void setId(int id) {
+         this.id = id; 
+        }
+
+    public int getPatientId() {
+         return patientId; 
+        }
+
+    public void setPatientId(int patientId) {
+         this.patientId = patientId; 
+        }
+
+    public String getBloodGroup() { return bloodGroup; }
+
+    public void setBloodGroup(String bloodGroup) { this.bloodGroup = bloodGroup; }
+
+    public int getQuantity() { return quantity; }
+
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+
+    public String getUrgency() { return urgency; }
+
+    public void setUrgency(String urgency) { this.urgency = urgency; }
+
+    public String getStatus() { return status; }
+
+    public void setStatus(String status) { this.status = status; }
+
+    public String getRequestDate() { return requestDate; }
+
+    public void setRequestDate(String requestDate) { this.requestDate = requestDate; }
+
+    public String getPatientName() { return patientName; }
+
+    public void setPatientName(String patientName) { this.patientName = patientName; }
+
 }
