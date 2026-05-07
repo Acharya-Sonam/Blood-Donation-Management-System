3115b0a937bdfcef9c57da22a156665d755f7741
Nirjal <nirjalguragainofficial@gmail.com>
2026-05-07 06:41:54 -0700

    donation controller

--- src/main/java/com/blooddonationmanagementsystem/controller/DonationController.java
+++ src/main/java/com/blooddonationmanagementsystem/controller/DonationController.java
@@ -1,5 +1,18 @@
 package com.blooddonationmanagementsystem.controller;
 
-public class DonationController {
-    
-}
+package com.blooddonationmanagementsystem.controller;
+ 
+import com.blooddonationmanagementsystem.model.Donation;
+import com.blooddonationmanagementsystem.service.DonationService;
+import com.blooddonationmanagementsystem.service.DonorService;
+ 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+ 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
