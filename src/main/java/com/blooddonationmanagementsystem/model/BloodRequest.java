d7394651e913e02aed1f68f53c7a9826c9adbc39
diyasiwakoti-12 <diyasiwakoti59@gmail.com>
2026-05-10 12:50:12 +0545

    Refactor: Clean up indentation and add constructor to BloodRequest model

--- src/main/java/com/blooddonationmanagementsystem/model/BloodRequest.java
+++ src/main/java/com/blooddonationmanagementsystem/model/BloodRequest.java
@@ -1,7 +1,5 @@
 package com.blooddonationmanagementsystem.model;
 
-
-
 public class BloodRequest {
 
     private int id;
@@ -13,69 +11,77 @@ public class BloodRequest {
     private String requestDate;
     private String patientName;
 
+    public BloodRequest() {}
 
-    public int getId() { 
-        return id; 
+    public BloodRequest(int patientId, String bloodGroup, int quantity, String urgency, String status) {
+        this.patientId = patientId;
+        this.bloodGroup = bloodGroup;
+        this.quantity = quantity;
+        this.urgency = urgency;
+        this.status = status;
     }
 
-    public int getPatientId() {
-        return patientId; 
-        }
-        
-    public String getBloodGroup() { 
-        return bloodGroup; 
+    public int getId() {
+        return id;
     }
 
     public void setId(int id) {
-        this.id = id; 
-        }
- 
-    public int getQuantity() { 
-        return quantity; 
+        this.id = id;
+    }
+
+    public int getPatientId() {
+        return patientId;
     }
 
     public void setPatientId(int patientId) {
-        this.patientId = patientId; 
-        }
-
-    public void setBloodGroup(String bloodGroup) { 
-        this.bloodGroup = bloodGroup; 
+        this.patientId = patientId;
     }
 
-    public String getUrgency() { 
-        return urgency; 
+    public String getBloodGroup() {
+        return bloodGroup;
     }
 
-    public String getStatus() { 
-        return status; 
+    public void setBloodGroup(String bloodGroup) {
+        this.bloodGroup = bloodGroup;
     }
 
-    public String getRequestDate() { 
-        return requestDate; 
+    public int getQuantity() {
+        return quantity;
     }
 
-    public String getPatientName() { 
-        return patientName; 
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 
-    public void setQuantity(int quantity) { 
-        this.quantity = quantity; 
+    public String getUrgency() {
+        return urgency;
     }
 
-    public void setUrgency(String urgency) { 
-        this.urgency = urgency; 
+    public void setUrgency(String urgency) {
+        this.urgency = urgency;
     }
 
-    public void setStatus(String status) { 
-        this.status = status; 
+    public String getStatus() {
+        return status;
     }
 
-    public void setRequestDate(String requestDate) { 
-        this.requestDate = requestDate; 
+    public void setStatus(String status) {
+        this.status = status;
     }
 
-    public void setPatientName(String patientName) { 
-        this.patientName = patientName; 
+    public String getRequestDate() {
+        return requestDate;
     }
 
-}
+    public void setRequestDate(String requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+}
